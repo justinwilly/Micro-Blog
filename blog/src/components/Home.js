19ebadfc49fc21ea39c249ba804a3bb6ea8bb4c8
Justin C Williams <45780143+justinwilly@users.noreply.github.com>
2021-01-07 11:50:36 -0500

    useState with onClick function

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -1,24 +1,35 @@
+import { useState } from 'react'
+
 const Home = () => {
 
+    const [name, setName] = useState('Justin');
+
+    const [age, setAge] = useState(29);
 
     // E is an event object 
     const handleClick = (e) => {
+        setName('Jessica')
+        setAge(25)
         console.log("whats up, i was clicked", e)
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log("hello " + name, e.target)
-    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log("hello " + name, e.target)
+    // }
 
 
     return (
         <div className="home">
             <h1>Home</h1>
-
+            <p>{ name } is { age} </p>
             {/* onClick function */}
             <button onClick={handleClick}> Click me </button>
+            
+            
+
+
             {/* arrow function for onClick */}
-            <button onClick={(e) => handleClickAgain('justin', e)}> Click me now</button>
+            {/* <button onClick={(e) => handleClickAgain('justin', e)}> Click me now</button> */}
         </div>
     );
 }
